e87f367a947c50b6ece793b3a1632880d5f2fb94
Popastack <114974188+opencontri@users.noreply.github.com>
2022-10-15 21:12:53 +0530

    Revert "Altered the required font color"

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ app.geometry("532x632")
 app.title("Stable Bud") 
 ctk.set_appearance_mode("dark") 
 
-
-prompt = ctk.CTkEntry(height=40, width=512, text_font=("Arial", 20), text_color="blue", fg_color="white") 
 prompt = ctk.CTkEntry(height=40, width=512, text_font=("Calibri", 40), text_color="black", fg_color="white") 
-
 prompt.place(x=10, y=10)
 
 lmain = ctk.CTkLabel(height=512, width=512)
@@ -39,10 +36,7 @@ def generate():
     lmain.configure(image=img) 
     
 
-
-trigger = ctk.CTkButton(height=40, width=120, text_font=("Arial", 20), text_color="blue", fg_color="black", command=generate) 
 trigger = ctk.CTkButton(height=40, width=120, text_font=("Calibri", 40), text_color="white", fg_color="blue", command=generate) 
-
 trigger.configure(text="Generate") 
 trigger.place(x=206, y=60) 
 
